f28f0e6e2f90cb10a77772bfbace740af76bdd18
Morpilou <Morpilou@users.noreply.github.com>
2020-05-13 17:17:59 +0200

    fix gap between balls

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,16 @@
 // ----------------- GÉNÉRATEUR --------------------------- //
 
 // une unité = 1 seconde
-let timer = 1;
-let randomiser = {
+var timer = 1;
+var randomiser = {
   min: 50,
   max: 100
 };
-let coneSize;
+var randomSize = {
+  min: 130,
+  max: 180
+};
 var colors = ['343,58,85', '23,40,38', '61,16,100', '74,100,89', '44,90,90', '129,47,57'];
-var heights = ['0', '100', '200'];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
@@ -18,29 +20,14 @@ function setup() {
 }
 
 function draw() {
-  let coneSize = random(50, 100);
-  let y = 0;
-
+  var coneSize = random(50, 100);
   // Conditionellle nous permettant de savoir si la souris est appuyée
   // et si le timer est écoulé
   if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
     clear();
 
-    let x = random(0, 10);
-
-    if (x <= 5) {
-      for (var i = 0; i < random(0, 3); i = i + 1) {
-        draw_circle(width / 2, height / 2 - y, random(100, 200));
-        y = y + 100;
-      }
-    } else {
-      draw_circle(width / 2, height / 2 - random(randomiser.min, randomiser.max), random(150, 200));
-      draw_circle(width / 2 - random(randomiser.min, randomiser.max), height / 2, random(150, 200));
-      draw_circle(width / 2 + random(randomiser.min, randomiser.max), height / 2, random(150, 200));
-    }
-
+    draw_all_icecreamballs(randomiser);
     // Appel de notre fonction generator
-
     fill(44, 41, 87);
     iceCone(width / 2 - coneSize, height / 2 + 50, width / 2 + coneSize, height / 2 + 50, width / 2, height / 2 + random(200, 400));
 
@@ -57,10 +44,26 @@ function draw_circle(x, y, size) {
 
 }
 
+function draw_all_icecreamballs(randomiser) {
+  var x = random(0, 100);
+  var y = 0;
+
+  if (x <= 50) {
+    for (var i = 0; i < random(0, 3); i = i + 1) {
+      draw_circle(width / 2, height / 2 - y, random(randomSize.min, randomSize.max));
+      y = y + 100;
+    }
+  } else {
+    draw_circle(width / 2, height / 2 - random(randomiser.min, randomiser.max), random(randomSize.min, randomSize.max));
+    draw_circle(width / 2 - random(randomSize.min, randomSize.max) / 2.5, height / 2, random(randomSize.min, randomSize.max));
+    draw_circle(width / 2 + random(randomSize.min, randomSize.max) / 2.5, height / 2, random(randomSize.min, randomSize.max));
+  }
+}
+
 function random_color(colors) {
   var rand = round(random(colors.length - 1));
   var thisColors = colors[rand];
-  let splitColors = thisColors.split(",")
+  var splitColors = thisColors.split(",")
   fill(splitColors[0], splitColors[1], splitColors[2]);
 }
 
